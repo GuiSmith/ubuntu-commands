a48d80a5d5d33a8b83fb44e1e80734cfe229d2f2
GuiSmith <guilhermessmith2014@gmail.com>
2025-01-15 15:45:45 -0300

    explorer alias added

---
+++ explorer.sh
@@ -0,0 +1,7 @@
+# Execute $SHELL para saber qual shell está usando
+# Edite o arquivo source do shell
+# nano ~/.bashrc para $SHELL = /bin/bash
+# nano ~/.zshrc para $SHELL = /bin/zsh
+# Insira isso no final do arquivo e salve > alias explorer='xdg-open .'
+# execute source ~/.bashrc ou ~/.zshrc, dependendo de seu shell
+# feito
